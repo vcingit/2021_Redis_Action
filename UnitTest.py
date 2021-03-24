e324f269318a0c1b0b671986daf796984b0df14a
vcingit <2434509740@qq.com>
2021-03-24 15:58:10 +0800

    add C2 unit

--- UnitTest.py
+++ UnitTest.py
@@ -37,6 +37,36 @@ def UNIT_TEST_VOTEARTICLE():
 
     UNIT_TEST_GetGroupTopArticles(conn, "B", 1, ["article:1", "article:2"])
 
+@RunFunc
+def UNIT_TEST_SHOPPINGCART():
+    from test.U2_ShoppingCart import UNIT_TEST_UpdateToken
+    from test.U2_ShoppingCart import UNIT_TEST_CheckToken
+    from test.U2_ShoppingCart import UNIT_TEST_SetItemNumToCart
+    from test.U2_ShoppingCart import UNIT_TEST_CacheRequest
+    from test.U2_ShoppingCart import UNIT_TEST_CacheRows
+    from test.U2_ShoppingCart import UNIT_TEST_CacheViews
+    
+    for i in range(0,31):
+        UNIT_TEST_UpdateToken(conn, i, "item_xxx")
+        if i == 30:
+            #when add item31, check del item1
+            UNIT_TEST_CheckToken(conn, "abcdefghijklmn_token" + str(i - 30), False)
+            #check item2 exist
+            UNIT_TEST_CheckToken(conn, "abcdefghijklmn_token" + str(i - 29), True)
+            #check item30 exist
+            UNIT_TEST_CheckToken(conn, "abcdefghijklmn_token" + str(i), True)
+    #set item 3 and check 3
+    UNIT_TEST_SetItemNumToCart(conn, "abcdefghijklmn_token0", "item_xxx0", 3)
+    #delete item and check exist
+    UNIT_TEST_SetItemNumToCart(conn, "abcdefghijklmn_token0", "item_xxx0", -1)
+
+    #check cache request expire
+    UNIT_TEST_CacheRequest(conn, "test_request")
+    #check cache rows
+    UNIT_TEST_CacheRows(conn)
+    #check cache views
+    UNIT_TEST_CacheViews(conn)
+
 def BeforeTest():
     #call = raw_input().split()
     #funcs[call[0]](*call[1:])
@@ -45,7 +75,7 @@ def BeforeTest():
     #test start, init db
 
 def AfterTest():
-    #test end, wait a faw time
+    #test end, wait a few seconds
     cnt = 0
     while True:
         time.sleep(1)
@@ -59,5 +89,7 @@ if __name__ == "__main__":
     BeforeTest()
 
     UNIT_TEST_VOTEARTICLE()
+
+    UNIT_TEST_SHOPPINGCART()
 
     AfterTest()
